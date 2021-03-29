a26aab69e200f6e7ad2bd88a4015fbeeec98fa9a
4332weizi <4332weizi@gmail.com>
2021-03-29 15:07:55 +0800

    变更打包流程

---
+++ .github/workflows/merge-package.js
@@ -0,0 +1,23 @@
+// node merge-package.js ./shell-package.json ./caculator-package.json ./package.json
+// taro工程中的package.json
+var shell = process.argv[2]
+// 壳工程中的package.json
+var self = process.argv[3]
+// 输出的package.json
+var dest = process.argv[4]
+
+console.log('merge %s %s into %s', from, to, dest)
+
+var fs = require('fs');
+var shellJson = require(shell)
+var selfJson = require(self)
+
+// 合并依赖
+Object.keys(selfJson.dependencies).forEach((v) => {
+  if (!shellJson.devDependencies[v] && !shellJson.dependencies[v]) {
+    shellJson.dependencies[v] = selfJson.dependencies[v]
+  }
+})
+
+// 合并结果输出到文件中
+fs.writeFileSync(dest, JSON.stringify(shellJson, null, 2))
